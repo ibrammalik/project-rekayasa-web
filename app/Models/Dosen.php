8610bbdbb19149d84033ec001ca860a75c193da6
ibrammalik <malikmaul1173@gmail.com>
2024-12-07 10:20:08 +0700

    ubah nama tabel

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dosen extends Model
 {
+    protected $table = 'dosen';
     protected $fillable = ['nip', 'nama', 'telp', 'email'];
 }
--- app/Models/Makul.php
+++ app/Models/Makul.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Makul extends Model
 {
+    protected $table = 'makul';
     protected $fillable = ['kode', 'nama', 'sks', 'semester'];
 }
